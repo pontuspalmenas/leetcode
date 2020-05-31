5b97b004d9066149698d8189c852b0b93b92c8ab
Pontus Palmenäs <pontus@palmenas.se>
2020-05-31 21:00:30 +0200

    1560/1563 test cases passed

--- java/src/main/java/se/pikzel/leetcode/problems/_2_add_two_numbers/Problem2Solution.java
+++ java/src/main/java/se/pikzel/leetcode/problems/_2_add_two_numbers/Problem2Solution.java
@@ -4,40 +4,57 @@ package se.pikzel.leetcode.problems._2_add_two_numbers;
 
 public class Problem2Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return new ListNode();
+        long sum = listToNumber(l1) + listToNumber(l2);
+        return numberToReversedList(sum);
     }
 
-    private int extractNumber(ListNode node) {
-        return -1;
-    }
-
-    public ListNode reverse(ListNode node) {
-        ListNode curr = node;
-        ListNode prev = null;
-        ListNode next;
-
-        while (curr != null) {
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-
-        return prev;
-    }
-
-    public ListNode numberToList(int number) {
-        ListNode node = new ListNode();
-        ListNode prev = null;
-        int len = (int)Math.log10(number);
-        for (int i = len; i >= 0; i--) {
-            int part = number / (int)Math.pow(10, i);
-            number -= part * (int)Math.pow(10, i);
-
-            node.val = part;
-            node.next = new ListNode();
+    private ListNode numberToReversedList(long number) {
+        if (number == 0) return new ListNode(0);
+        ListNode node = null;
+        while (number > 0) {
+            int digit = (int)(number % 10);
+            node = addNode(node, new ListNode(digit));
+            number = number / 10;
         }
 
         return node;
+    }
+
+    private long listToNumber(ListNode node) {
+        long sum = 0;
+        int i = 0;
+        ListNode p = node;
+        while (p != null) {
+            sum += p.val * powerN(10, i);
+            p = p.next;
+            i++;
+        }
+
+        return sum;
+    }
+
+    long powerN(long number, int power){
+        long res = 1;
+        long sq = number;
+        while(power > 0){
+            if(power % 2 == 1){
+                res *= sq;
+            }
+            sq = sq * sq;
+            power /= 2;
+        }
+        return res;
+    }
+
+    private ListNode addNode(ListNode head, ListNode n) {
+        ListNode p = head;
+        if (head == null) return n;
+        else {
+            while (p.next != null) {
+                p = p.next;
+            }
+            p.next = n;
+        }
+        return head;
     }
 }
--- java/src/test/java/se/pikzel/leetcode/problems/_2_add_two_numbers/Problem2SolutionTest.java
+++ java/src/test/java/se/pikzel/leetcode/problems/_2_add_two_numbers/Problem2SolutionTest.java
@@ -6,23 +6,88 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Problem2SolutionTest {
     @Test
-    void reverseList() {
-        var input = new ListNode(1, new ListNode(2));
-        var reverse = new Problem2Solution().reverse(input);
-        dump(reverse);
-        assertEquals(2, reverse.val);
-        assertEquals(1, reverse.next.val);
+    void defaultCase() {
+        var l1 = arrToList(new int[]{2,4,3});
+        var l2 = arrToList(new int[]{5,6,4});
+
+        var sum = new Problem2Solution().addTwoNumbers(l1,l2);
+
+        assertEquals("[7,0,8]",listToString(sum));
     }
 
     @Test
-    void fisk() {
-        dump(new Problem2Solution().numberToList(123));
+    void zeroCase() {
+        var l1 = new ListNode(0);
+        var l2 = new ListNode(0);
+
+        var sum = new Problem2Solution().addTwoNumbers(l1,l2);
+
+        assertEquals(0, sum.val);
+    }
+
+    @Test
+    void differentLengths() {
+        var l1 = arrToList(new int[]{9});
+        var l2 = arrToList(new int[]{1,9,9,9,9,9,9,9,9,9});
+
+        var sum = new Problem2Solution().addTwoNumbers(l1,l2);
+        assertEquals("[0,0,0,0,0,0,0,0,0,0,1]", listToString(sum));
+    }
+
+    @Test
+    void case1561() {
+        var l1 = arrToList(new int[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1});
+        var l2 = arrToList(new int[]{5,6,4});
+
+        var sum = new Problem2Solution().addTwoNumbers(l1,l2);
+        dump(sum);
+        assertEquals("[6,6,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]", listToString(sum));
+    }
+
+    @Test
+    void arrToListTest() {
+        var l1 = arrToList(new int[]{1,2,3});
+        var l2 = arrToList(new int[]{4,5});
+
+        assertEquals(1, l1.val);
+        assertEquals(2, l1.next.val);
+        assertEquals(3, l1.next.next.val);
+        assertEquals(4, l2.val);
+        assertEquals(5, l2.next.val);
+    }
+
+    private ListNode arrToList(int[] arr) {
+        ListNode list = null;
+        for (var i : arr) {
+            list = addNode(list, new ListNode(i));
+        }
+        return list;
+    }
+
+    private ListNode addNode(ListNode head, ListNode n) {
+        ListNode p = head;
+        if (head == null) return n;
+        else {
+            while (p.next != null) {
+                p = p.next;
+            }
+            p.next = n;
+        }
+        return head;
     }
 
     private void dump(ListNode n) {
+        System.out.println(listToString(n));
+    }
+
+    private String listToString(ListNode n) {
+        String s = "[";
         while (n != null) {
-            System.out.print("" + n.val + " -> ");
+            s += n.val;
+            if (n.next != null) s += ",";
             n = n.next;
         }
+        s += "]";
+        return s;
     }
 }
